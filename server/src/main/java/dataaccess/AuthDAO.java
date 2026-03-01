a4799ec084cc688e4ea0157875cd610dc56fd9d7
etarnett <arnett.elijah22@gmail.com>
2026-03-01 11:45:31 -0700

    Make the DAO interfaces for User, Game, and Auth using all the variables needed in each

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+import model.AuthData;
+
+import javax.xml.crypto.Data;
+
+public interface AuthDAO {
+    void clear() throws DataAccessException;
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+}
---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.*;
+
+public interface GameDAO {
+    void clear() throws DataAccessException;
+    int createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+}
