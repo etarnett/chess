606167e0f401e6905e02e012b24593c5a7431e9e
etarnett <arnett.elijah22@gmail.com>
2026-03-11 20:24:50 -0600

    Used the clear, insert user, and get user to implement the same in the authDAO

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -1,23 +1,62 @@
 package dataaccess;
 
 import model.AuthData;
+import model.UserData;
+
+import java.sql.SQLException;
 
 public class MySqlAuthDAO implements AuthDAO {
 
-    public MySqlAuthDAO() throws DataAccessException {
+    public MySqlAuthDAO() {
     }
 
     @Override
     public void clear() throws DataAccessException {
+        var sql = "DELETE FROM auth";
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to clear auth tables", ex);
+        }
     }
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
+        var sql = "INSERT INTO user (authToken, username) VALUES (?, ?)";
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, auth.authToken());
+            ps.setString(2, auth.username());
+
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to create auth", ex);
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        var sql = "SELECT authToken, username FROM auth WHERE authToken=?";
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, authToken);
+            try (var rs = ps.executeQuery()) {
+                //check if line exists
+                if (rs.next()) {
+                    String token = rs.getString("authToken");
+                    String username = rs.getString("username");
+
+                    return new AuthData(token, username);
+                }
+                return null;
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to get auth", ex);
+        }
     }
 
     @Override
