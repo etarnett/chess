70f5a8720a99448cbfa11421878af9cde14854e5
etarnett <arnett.elijah22@gmail.com>
2026-03-23 21:43:36 -0600

    Update ClientMain to handle commands and print a simple help menu

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,52 @@
 package client;
 
 import chess.*;
+import java.util.*;
 
 public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        new ClientMain().run();
     }
+
+    private final Scanner scanner = new Scanner(System.in);
+    private boolean running = true;
+
+    public void run() {
+        while (running) {
+            printPrompt();
+            String input = scanner.nextLine();
+            handleCommand(input);
+        }
+
+        System.out.println("Goodbye!");
+    }
+
+    private void printPrompt() {
+        System.out.print("> ");
+    }
+
+    private void handleCommand(String input) {
+        String command = input.trim().toLowerCase();
+
+        switch (command) {
+            case "help" -> printHelp();
+            case "quit" -> running = false;
+            default -> System.out.println("Unknown command. Type 'help' for options.");
+        }
+
+    }
+
+    private void printHelp() {
+        System.out.println("""
+                register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                login <USERNAME> <PASSWORD> - to play chess
+                quit - playing chess
+                help - with possible commands
+                """);
+    }
+
+
 }
---
+++ client/src/main/java/ui/PreloginUI.java
@@ -0,0 +1,9 @@
+package ui;
+
+import client.ServerFacade;
+import model.*;
+import java.util.*;
+
+public class PreloginUI {
+
+}
