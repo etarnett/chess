72988f68c3ebd091a11c078923e5df17d5d243f4
etarnett <arnett.elijah22@gmail.com>
2026-03-23 23:45:25 -0600

    Implementing a new class for the different actions for prelogin

--- client/src/main/java/ui/PreloginUI.java
+++ client/src/main/java/ui/PreloginUI.java
@@ -6,4 +6,43 @@ import java.util.*;
 
 public class PreloginUI {
 
+    private final ServerFacade server;
+    private final Scanner scanner;
+
+    public PreloginUI(ServerFacade server, Scanner scanner) {
+        this.server = server;
+        this.scanner = scanner;
+    }
+
+    public AuthData runCommand(String input) {
+        String command = input.trim().toLowerCase();
+
+        try {
+            return switch (command) {
+                case "help" -> {
+                    printHelp();
+                    yield null;
+                }
+                case "login" -> login();
+                case "register" -> register();
+                default -> {
+                    System.out.println("Unknown command. Type 'help' for options.");
+                    yield null;
+                }
+            };
+        } catch (Exception except) {
+            System.out.println("Error: " + except.getMessage());
+            return null;
+        }
+    }
+
+    private void printHelp() {
+        System.out.println("""
+                register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                login <USERNAME> <PASSWORD> - to play chess
+                quit - playing chess
+                help - with possible commands
+                """);
+    }
+
 }
