5d75100248bc8d2be33657a21102b60b3d544390
etarnett <arnett.elijah22@gmail.com>
2026-03-24 18:15:15 -0600

    started to set up the post login ui. copied structure from prelogin ui

---
+++ client/src/main/java/ui/PostloginUI.java
@@ -0,0 +1,86 @@
+package ui;
+
+import client.ServerFacade;
+import model.*;
+
+import java.util.*;
+
+
+public class PostloginUI {
+
+    private final ServerFacade server;
+    private final Scanner scanner;
+    private final String authToken;
+
+    public PostloginUI(ServerFacade server, Scanner scanner, String authToken) {
+        this.server = server;
+        this.scanner = scanner;
+        this.authToken = authToken;
+    }
+
+    public boolean runCommand(String input) {
+        String command = input.trim().toLowerCase();
+
+        try {
+            switch (command) {
+                case "help" -> printHelp();
+                case "logout" -> {
+                    server.logout(authToken);
+                    System.out.println("Logged out.");
+                    return true;
+                }
+                case "create" -> createGame();
+                case "list" -> listGames();
+                case "join" -> joinGame();
+                case "observe" -> observeGame();
+                default -> System.out.println("Unknown command. Type 'help' for options.");
+            }
+        } catch (Exception except){
+            System.out.println("Error: " + except.getMessage());
+        }
+
+        return false;
+    }
+
+    private void printHelp() {
+        System.out.println("""
+                create - a game
+                list - games
+                join - a game
+                observe - a game
+                logout - when you are done
+                quit - playing chess
+                help - with possible commands
+                """);
+    }
+
+    private AuthData login() throws Exception {
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        AuthData auth = server.login(username, password);
+        System.out.println("Logged in as " + auth.username());
+
+        return auth;
+    }
+
+    private AuthData register() throws Exception {
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        System.out.print("email: ");
+        String email = scanner.nextLine();
+
+        AuthData auth = server.register(username, password, email);
+        System.out.println("Registered and logged in as " + auth.username());
+
+        return auth;
+    }
+
+}
